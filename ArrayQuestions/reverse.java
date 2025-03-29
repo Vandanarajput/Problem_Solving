a38d5299f570c30f1e447cc8c8880dda6a13816c
Vandanarajput <vandanakashyap674@gmail.com>
2025-03-29 21:18:52 +0530

    reverse array

---
+++ ArrayQuestions/reverse.java
@@ -0,0 +1,11 @@
+package Problem_Solving.ArrayQuestions;
+//reverse the array
+public class reverse {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
